dcae8bc5e41da9e6949b6ba9666ded51a7eac228
Jonathan Kurten <kurtenj@gmail.com>
2018-05-31 18:24:15 -0700

    Remove extra font file, set proper margins on body, test direct emoji

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ import Twitter from '../assets/Twitter_Logo_Blue.svg'
 const IndexPage = () => (
   <div id="designBoiContent">
     <h1 className="yellingText">Howzit, I’m Jonathan</h1>
-    <p>I’m a regular old <span className="pinkyBoi">design boy</span><img className="emojiSVG" src={Paint} /> from <span className="wrongBoi">Cape Town</span> Portland. I’m sorta seriously into how people move around and I work with some dope folks to make <span className="blueBoi">urban mobility</span><img className="emojiSVG" src={Bus} /> better. <span className="grayBois">Now and then I jot some things down, mostly about design systems and product design process. Oh yeah, I’m a coffee snob (duh), hit me up below and we’ll grab a cup.</span></p>
+    <p>I’m a regular old <span className="pinkyBoi">design boy</span><img className="emojiSVG" src={Paint} /> from <span className="wrongBoi">Cape Town</span> Portland. I’m sorta seriously into how people move around and I work with some dope folks to make <span className="blueBoi">urban mobility</span><img className="emojiSVG" src={Bus} /> better. <span className="grayBois">Now and then I jot some things down, mostly about design systems and product design process. Oh yeah, I’m a coffee snob ☕ (duh), hit me up below and we’ll grab a cup.</span></p>
 
     <div id="fancyPantsSVG">
       <a
